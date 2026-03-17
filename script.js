bd73ebd3080bd8f2861358cd0c980214e63cc76d
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-03-17 17:15:43 -0300

    feat: adiciona a função carregarInvestimentosNaTela para adicionar os cards de tipos de investimentos na tela

--- script.js
+++ script.js
@@ -55,11 +55,13 @@ function adicionarInvestimento(nome,valor){
     
     tipos.push({nome:nome, valor:valor});
     somarInvestimentos();
+    carregarInvestimentosNaTela();
 };
 
 function removerInvestimento(indice){
     tipos.splice(indice, 1);
     somarInvestimentos();
+    carregarInvestimentosNaTela();
 };
 
 function adicionarValor(indice,valor){
@@ -77,6 +79,7 @@ function adicionarValor(indice,valor){
     
     tipos[indice].valor += valor;
     somarInvestimentos();
+    carregarInvestimentosNaTela();
 };
 
 function retirarValor(indice,valor){
@@ -94,6 +97,7 @@ function retirarValor(indice,valor){
     
     tipos[indice].valor -= valor;
     somarInvestimentos();
+    carregarInvestimentosNaTela();
 };
 
 async function getApi(){
@@ -141,10 +145,62 @@ function alteraTotal() {
     p.innerHTML = `R$ ${total.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 };
 
+function carregarInvestimentosNaTela() {
+    const secaoTipos = document.querySelector(".tipos");
+    secaoTipos.innerHTML = "";
+    if(tipos.length === 0) {
+        const h1 = document.createElement("h1");
+        h1.classList.add("adicione-primeiro");
+        h1.style.fontSize = "40px";
+        h1.textContent = "Adicione o primeiro investimento";
+        secaoTipos.append(h1);
+    } else {
+        tipos.forEach((tipo, indice) => {
+            const cardTipos = document.createElement("div");
+            const botoesAdicionarRetirar = document.createElement("div");
+            const botaoAdicionarValor = document.createElement("button");
+            const botaoRetirarValor = document.createElement("button");
+            const div = document.createElement("div");
+            const h2 = document.createElement("h2");
+            const p = document.createElement("p");
+            const botaoExcluir = document.createElement("button");
+            
+            cardTipos.classList.add("card-tipos")
+            botoesAdicionarRetirar.classList.add("botoes-adicionar-retirar");
+            botaoAdicionarValor.classList.add("adicionar-valor");
+            botaoAdicionarValor.addEventListener("click", () => adicionarValor(indice));
+            botaoAdicionarValor.textContent = "+";
+            botaoRetirarValor.classList.add("retirar-valor");
+            botaoRetirarValor.addEventListener("click", () => retirarValor(indice));
+            botaoRetirarValor.textContent = "-";
+            botaoExcluir.classList.add("botao-de-excluir");
+            botaoExcluir.addEventListener("click", () => removerInvestimento(indice));
+            botoesAdicionarRetirar.append(botaoAdicionarValor);
+            botoesAdicionarRetirar.append(botaoRetirarValor);
+            h2.textContent = tipo.nome;
+            p.textContent = `R$ ${tipo.valor.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+            div.append(h2);
+            div.append(p);
+            botaoExcluir.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                </svg>
+            `;
+            cardTipos.append(botoesAdicionarRetirar);
+            cardTipos.append(div);
+            cardTipos.append(botaoExcluir);
+            
+            secaoTipos.append(cardTipos)
+        });
+    }
+};
+
 
 document.addEventListener("DOMContentLoaded", function(){
     lerLocalStore();
     somarInvestimentos();
     configurarEventos();
     getApi();
+    carregarInvestimentosNaTela();
 });
