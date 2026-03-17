c8eff45ce87711f06e0ce8cd1228150072187d20
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-03-17 09:27:31 -0300

    refactor: altera a ordem das funções para garantir uma estrutura de código melhor

--- script.js
+++ script.js
@@ -2,25 +2,21 @@ let total = 0;
 let tipos = [];
 
 
-document.addEventListener("DOMContentLoaded", function(){
-    LerLocalStore();
-    Somarinvestimentos();
-    ConfigurarEventos();
-    getApi();
-});
-
-
 function LerLocalStore(){
     tipos = JSON.parse(localStorage.getItem("tipos"));
-
+    
     if(tipos == null){
         tipos = [];
     }
 };
 
+function SalvarLocalStorage(){
+    localStorage.setItem("tipos",JSON.stringify(tipos));
+};
+
 function adicionaInvestimento(nome,valor){
     valor = Number(valor);
-
+    
     if(nome.trim() === ""){
         alert("Digite um nome válido");
         return;
@@ -30,50 +26,55 @@ function adicionaInvestimento(nome,valor){
         alert("Digite um valor númerico válido");
         return;
     }
-  
+    
     for(let i = 0; i<tipos.length ; i++){
         if(tipos[i].nome === nome){
             alert("Esse investimento já foi adicionado");
             return;
         }
     }
-  
+    
     tipos.push({nome:nome, valor:valor});
+    Somarinvestimentos();
+};
+
+function RemoverInvestimento(indice){
+    tipos.splice(indice, 1);
     Somarinvestimentos();
 };   
 
 function Somarinvestimentos(){
     total = 0;
-
+    
     tipos.forEach(function(item){
         total += item.valor;
         console.log(item.valor);
         console.log(total);
     })
-
+    
     SalvarLocalStorage();
 };
 
 function AdicionarValor(indice,valor){
     valor = Number(valor);
-
+    
     if(isNaN(valor) || valor <= 0){
         alert("Digite um valor válido");
         return;
     }
-
+    
     if(indice < 0 || indice >= tipos.length){
         alert("Investimento Inválido");
         return;
     }
-
+    
     tipos[indice].valor += valor;
     Somarinvestimentos();
 };
 
 function RetirarValor(indice,valor){
     valor = Number(valor);
-
+    
     if(indice < 0 || indice >= tipos.length){
         alert("Investimento Inválido");
         return;
@@ -83,18 +84,9 @@ function RetirarValor(indice,valor){
         alert("Valor maior que o investimento");
         return;
     }
-
+    
     tipos[indice].valor -= valor;
     Somarinvestimentos();
-};
-
-function RemoverInvestimento(indice){
-    tipos.splice(indice, 1);
-    Somarinvestimentos();
-};
-
-function SalvarLocalStorage(){
-    localStorage.setItem("tipos",JSON.stringify(tipos));
 };
 
 async function getApi(){
@@ -119,19 +111,26 @@ function ConfigurarEventos(){
     let botaoNovo = document.querySelector(".botao-novo-tipo");
     let botaoReais = document.querySelector(".botao-reais");
     let botaoDolar = document.querySelector(".botao-dolares");
-
+    
     botaoNovo.addEventListener("click", function(){
         let nome = prompt("Nome do investimento:");
         let valor = prompt("Valor do investimento");
-
+        
         adicionaInvestimento(nome, valor);
     });
-
+    
     botaoReais.addEventListener("click", function(){
         console.log("Modo reais");
     });
-
+    
     botaoDolar.addEventListener("click", function(){
         console.log("Modo dólar");
     });
 };
+
+document.addEventListener("DOMContentLoaded", function(){
+    LerLocalStore();
+    Somarinvestimentos();
+    ConfigurarEventos();
+    getApi();
+});
